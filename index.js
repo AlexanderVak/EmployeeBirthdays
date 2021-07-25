10cfb8834f049beaaacbce72d70c68df96893030
Olexandr Vakulinskiy <mamorunokitsune@gmail.com>
2021-07-26 01:02:22 +0300

    sort employees by days and months

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 import moment from 'moment';
-import 'moment/locale/uk';
+import 'moment/locale/uk.js';
 moment.locale('uk')
 export const input = [
-    { name: 'Джонатан Джостар', birthday: moment("1986-04-04")},
+    { name: 'Джорно Джованна', birthday: moment('2000-04-16') },
     { name: 'Джозеф Джостар', birthday: moment('1996-09-27') },
     { name: 'Куджо Джотаро', birthday: moment('2000-05-10') },
-    { name: 'Джорно Джованна', birthday: moment('2000-04-16') },
+    { name: 'Джонатан Джостар', birthday: moment("1986-04-04")},
+    { name: 'Бруно Бучаратті', birthday: moment('2000-07-22') },
     { name: 'Діо Брандо', birthday: moment('1986-08-12') },
 ]
 
 export function age(input){
     let today = moment()
-    let birthday = input[0].birthday
+    let birthday = input[3].birthday
     let age = today.diff(birthday, 'years')
     return age
 }
 
-export function groupByMonths(input){
+export function employeesGroupedByMonths(input){
     const employees = new Map()
+
     input.forEach(employee => {
         if (employees.has(employee.birthday.month())) {
             employees.get(employee.birthday.month()).push(employee)
@@ -29,5 +31,36 @@ export function groupByMonths(input){
 
     return employees
 }
+export function employeesSortedByDay(employees) {
+    employees.forEach((employeesByMonth) => employeesByMonth.sort((a, b) => a.birthday - b.birthday))
+    return employees
+}
+export function planningHorizon(horizon, employees) {
+    let currentMonth = moment().month()
+    switch (horizon) {
+        case 0:
+            return employees.get(currentMonth)
+        case 1:
+            return [employees.get(currentMonth), employees.get(currentMonth + 1)]
+        case 2:
+            return [employees.get(currentMonth), employees.get(currentMonth + 1), employees.get(currentMonth + 2)]    
+        default:
+            break;
+    }
+}
+export function showListOfEmployeesBirthdays(employees){
+    let currentDate = moment()
+    var futureDate = moment()
+    for (let i = 0; i < employees.length; i++) {
+        for (let j = 0; j < employees[i].length; j++) {
+            `${futureDate.format("MMMM YYY")}\n${}`
+            
+        }
+        futureDate = moment(currentDate).add(1, 'M');
+        
+    }
+    return `${moment().year().month().format("MMMM YYY")}`
 
-console.log(input[0].birthday);
+}
+
+console.log(showListOfEmployeesBirthdays(planningHorizon(2, employeesSortedByDay(employeesGroupedByMonths(input)))));
--- index.test.js
+++ index.test.js
@@ -1,13 +1,22 @@
-import {input, age, groupByMonths} from './index';
+import {input, age, employeesGroupedByMonths, employeesSortedByDay, planningHorizon} from './index';
 describe('employee bitydays', () => {
     it('should show employee`s birthday', () => {
-        expect(`${input[0].name} ${input[0].birthday.format("D MMMM")}`).toBe('Джонатан Джостар 4 квітня')                        
+        expect(`${input[3].name} ${input[3].birthday.format("D MMMM")}`).toBe('Джонатан Джостар 4 квітня')                        
     });
     it('should get employee`s age', () => {
         expect(age(input)).toBe(35)
     });
     it('should group employees by months', () => {
-        expect(groupByMonths(input).get(3)).toEqual([input[0], input[3]])
+        expect(employeesGroupedByMonths(input).get(3)).toEqual([input[0], input[3]])
+    });
+    it('should show employees by current month', () => {
+        expect(employeesGroupedByMonths(input).get(6)).toEqual([input[4]])
+    });
+    it('should sort employees by day of birth in curr month', () => {
+        expect(employeesSortedByDay(employeesGroupedByMonths(input)).get(3)).toEqual([input[3], input[0]])
+    });
+    it('should return employees by planning horizon 2', () => {
+        expect(planningHorizon(2, employeesSortedByDay(employeesGroupedByMonths(input)))).toEqual([[input[4]], [input[5]], [input[1]]])
     });
 
 });
